a032886776a0a4a613591a784a375a96a05cf83e
Tom Chen <tom@itri.org.tw>
2019-02-26 21:23:31 +0800

    wip: exercise Enum.sum func

--- 05concat.exs
+++ 05concat.exs
@@ -1,9 +1,15 @@
-
 defmodule Concat do
   def join(a, b, sep \\ " ") do
     a <> sep <> b
   end
+
+  def sumMe do
+    # Enum.find
+    Enum.sum([100, 200, 300])
+  end
 end
 
-IO.puts Concat.join("Hello", "World")
-IO.puts Concat.join("Hello", "World", "_")
+IO.puts(Concat.join("Hello", "World"))
+IO.puts(Concat.join("Hello", "World", "_"))
+
+IO.puts(Concat.sumMe())
