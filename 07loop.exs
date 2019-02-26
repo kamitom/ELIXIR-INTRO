0616cb75ef2e34813fc40122b8c8a776b128ed20
Tom Chen <tom@itri.org.tw>
2019-02-26 22:08:36 +0800

    wip: initial exercise recursion

---
+++ 07loop.exs
@@ -0,0 +1,13 @@
+defmodule Math do
+  # sum 1+2+5
+  [1, 2, 5]
+
+  def sum_list([], acc), do: acc
+
+  def sum_list([head | tail], acc) do
+    sum_list(tail, acc + head)
+  end
+end
+
+IO.puts(Math.sum_list([1, 2, 5], 0))
+IO.puts(Math.sum_list([], 0))
