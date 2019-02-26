3373186e5586111638bc0810b56509adaf3302c7
Tom Chen <tom@itri.org.tw>
2019-02-26 22:10:44 +0800

    style: small modification

--- 07loop.exs
+++ 07loop.exs
@@ -1,13 +1,16 @@
 defmodule Math do
-  # sum 1+2+5
+  # GOAL: sum them ALL: 1+2+5
   [1, 2, 5]
 
   def sum_list([], acc), do: acc
 
+  # pratice recursion
   def sum_list([head | tail], acc) do
     sum_list(tail, acc + head)
   end
 end
 
+# prints: 8
 IO.puts(Math.sum_list([1, 2, 5], 0))
+# prints: 0
 IO.puts(Math.sum_list([], 0))
